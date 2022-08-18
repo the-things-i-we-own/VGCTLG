d01c46688efd67831ff42949db157006dac5e5fa
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-18 21:21:31 +0900

    Update index.php

--- index.php
+++ index.php
@@ -186,10 +186,10 @@ fclose($fp);
             color: var(--text-color);
         }
         
-        header label {
-            color: var(--org-text);
-            background-color: var(--org-bg);
-            border: var(--org-border);
+        ._more,
+        header label,
+        footer a {
+            color: var(--hover-color);
         }
         
         header {
