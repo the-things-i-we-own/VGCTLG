6f712f5713b154a9628dd96fc434fb605e617f3d
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-18 18:20:35 +0900

    vgctlg

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 function h($str) {
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
-$id = (string)filter_input(INPUT_POST, 'id');
-$name = (string)filter_input(INPUT_POST, 'name');
-$profile = (string)filter_input(INPUT_POST, 'profile');
-$language = (string)filter_input(INPUT_POST, 'language');
-$status = (string)filter_input(INPUT_POST, 'status');
+$org = (string)filter_input(INPUT_POST, 'org');
+$size = (string)filter_input(INPUT_POST, 'size');
+$img = (string)filter_input(INPUT_POST, 'img');
+$title = (string)filter_input(INPUT_POST, 'title');
+$text = (string)filter_input(INPUT_POST, 'text');
 
-$fp = fopen('index.csv', 'a+b');
+$fp = fopen('img.csv', 'a+b');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     flock($fp, LOCK_EX);
-    fputcsv($fp, [$id, $name, $profile, $language, $status, $questions,]);
+    fputcsv($fp, [$org, $size, $img, $title, $text]);
     rewind($fp);
 }
 
@@ -23,84 +23,411 @@ flock($fp, LOCK_UN);
 fclose($fp);
 
 ?>
+<!DOCTYPE html>
 <html lang="ja">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>VG BOOK | Very Gois</title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://creative-community.space/coding/js/org.js"></script>
-<script type="text/javascript">
-$(function(){
-$("#").load("");
-})
-</script>
-<link rel="stylesheet" href="/css/ctlg.css"/>
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
-<style type="text/css">
-    #cover {position:fixed; width:100vw; height:100vh; z-index:-1;}
-</style>
-</head>
-<body>
-<div id="cover">
-<h1><span>VG BOOK</span></h1>
-</div>
-<form id="org">
-<div class="search-box language">
-<ul>
-<li>
-<input type="radio" name="language" value="art" id="art">
-<label for="art" class="label">Art</label></li>
-<li>
-<input type="radio" name="language" value="music" id="music">
-<label for="music" class="label">Music</label></li>
-<li>
-<input type="radio" name="language" value="other" id="other">
-<label for="other" class="label">Etc,</label></li>
-</ul>
-</div>
-<div class="search-box status">
-<ul>
-<li>
-<input type="radio" name="status" value="people" id="people">
-<label for="people" class="label">People</label></li>
-<li>
-<input type="radio" name="status" value="group" id="group">
-<label for="group" class="label">Group</label></li>
-<li>
-<input type="radio" name="status" value="publisher" id="publisher">
-<label for="publisher" class="label">Label / Publisher</label></li>
-</ul>
-</div>
-<div class="reset">
-<input type="reset" name="reset" value="RESET" class="reset-button">
-</div>
-</form>
 
-<ul class="list">
-<?php if (!empty($rows)): ?>
-<?php foreach ($rows as $row): ?>
-<li class="list_item list_toggle" data-language="<?=h($row[3])?>" data-status="<?=h($row[4])?>" onclick="obj=document.getElementById('<?=h($row[0])?>').style; obj.display=(obj.display=='none')?'block':'none';">
-<span><?=h($row[1])?></span>
-<span id="<?=h($row[0])?>" class="text" style="display:none; clear:both;">
-<i id="profile"><?=h($row[2])?></i>
-<p class="buy">
-<a href="/ctlg/book/<?=h($row[0])?>">View More</a>
-</p>
-</span>
-</li>
-<?php endforeach; ?>
-<?php else: ?>
-<li class="list_item list_toggle" data-language="<?=h($row[3])?>" data-status="<?=h($row[4])?>" onclick="obj=document.getElementById('<?=h($row[0])?>').style; obj.display=(obj.display=='none')?'block':'none';">
-<span>Name</span>
-<span id="<?=h($row[0])?>" class="text" style="display:none; clear:both;">
-<i id="profile">Profile</i>
-<a href="/ctlg/book/<?=h($row[0])?>">View More</a>
-</span>
-</li>
-<?php endif; ?>
-</ul>
+<head>
+    <title>Title | The Things I (We) Own</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="author" content="creative-community.space">
+    <meta name="description" content="___">
+
+    <meta property="og:title" content="___" />
+    <meta property="og:description" content="___" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="___" />
+    <meta property="og:site_name" content="creative-community.space" />
+    <meta property="og:image" content="https://creative-community.space/org/qr.png" />
+    <meta property="og:locale" content="ja_JP" />
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@NLC_update" />
+    <meta name="twitter:image" content="https://creative-community.space/org/qr.png" />
+
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://creative-community.space/coding/js/tone/jquery.min.js"></script>
+    <script src="https://creative-community.space/coding/js/tone/jquery-ui.min.js"></script>
+    <script src="https://creative-community.space/coding/js/tone/Tone.min.js"></script>
+    <script src="https://creative-community.space/coding/js/tone/StartAudioContext.js"></script>
+    <script src="https://creative-community.space/coding/js/mousedragscrollable/scrollable.js"></script>
+    <script src="https://creative-community.space/org/searchBox.js"></script>
+    <script src="index.js"></script>
+
+    <link rel="stylesheet" href="https://creative-community.space/org/index.css" />
+    <link rel="stylesheet" href="https://creative-community.space/org/searchBox.css" />
+    <link rel="stylesheet" href="index.css" />
+    <link rel="stylesheet" href="cover.css" />
+    <style>
+        :root {
+            --text-color: red;
+            --text-family: "Courier New", Courier, monospace;
+            --hover-color: blue;
+            --border-style: 1px dashed green;
+            --bg-color: yellow;
+            --list-bg: transparent;
+            --list-text: green;
+            --list-family: "Courier New", Courier, monospace;
+            --org-text: blue;
+            --org-bg: green;
+            --org-border: solid 1px blue;
+            --update-text: #fff;
+        }
+        
+        #greeting #text,
+        #collection li p b,
+        h1,
+        h2,
+        .collection h2 {
+            font-family: var(--text-family)
+            font-weight: 500;
+        }
+        
+        #server p,
+        #collection li p,
+        form,
+        .reset-button,
+        .collection p,
+        marquee {
+            font-family: var(--list-family)
+        }
+        
+        #about {
+            width: 35rem;
+            max-width: 95%;
+        }
+
+        #img {
+            width: 55rem;
+            max-width: 75%;
+        }
+        
+        .change .mousedragscrollable {
+            display: block;
+        }
+
+        #bought:checked~label,
+        #gift:checked~label,
+        #free:checked~label,
+        #made:checked~label,
+        #collaborations:checked~label,
+        #other:checked~label,
+        #sale:checked~label {
+            text-decoration: double underline;
+        }
+        
+        #server p {
+            color: var(--update-text);
+        }
+        
+        #server:hover p {
+            text-shadow: 1px 1px 2px #fff, 0 0 1em #fff, 0 0 0.2em #fff;
+        }
+        
+        .change .mousedragscrollable::-webkit-scrollbar-thumb,
+        .change .mousedragscrollable li::-webkit-scrollbar-thumb {
+            background: var(--list-bg);
+            border: var(--border-style);
+        }
+        
+        #cover {
+            background-image: url("");
+            background-position: center;
+            background-size: auto 100%;
+            background-repeat: no-repeat;
+            overflow: hidden;
+        }
+        
+        #images {
+            position: absolute;
+            top: 40%;
+            left: 50%;
+            width: 90%;
+            height: 0;
+            -webkit-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+            max-width: 100vh;
+        }
+        
+        #images .list_item {
+            position: relative;
+            padding: 0;
+            margin: 2.5vh 0;
+        }
+
+        #images li:nth-child(n+26) {
+            display: none;
+        }
+        
+        #images img {
+            animation: 100s linear infinite spot;
+        }
+
+        @keyframes spot {
+            0% {
+                filter: drop-shadow(1rem 1rem 1rem rgba(50, 50, 50, 0.75));
+            }
+            25% {
+                filter: drop-shadow(1rem -0.5rem 1rem rgba(50, 50, 50, 0.75));
+            }
+            50% {
+                filter: drop-shadow(-1rem -1rem 1.5rem rgba(50, 50, 50, 0.75));
+            }
+            75% {
+                filter: drop-shadow(-0.5rem 2rem 2rem rgba(50, 50, 50, 0.75));
+            }
+            100% {
+                filter: drop-shadow(1rem 1rem 1rem rgba(50, 50, 50, 0.75));
+            }
+        }
+
+        body,
+        ._more:hover,
+        header label:hover,
+        footer a:hover,
+        #greeting p,
+        #server:hover p,
+        #collection li {
+            color: var(--text-color);
+        }
+
+        #main {
+            background-color: var(--bg-color);
+        }
+        
+        ._more,
+        header label,
+        footer a {
+            color: var(--hover-color);
+        }
+        
+        header marquee:hover {
+            color: #fff;
+            background: var(--bg-color);
+        }
+        
+        header,
+        header marquee {
+            border-bottom: var(--border-style);
+        }
+        
+        footer {
+            border-top: var(--border-style);
+        }
+        
+        ._more:hover,
+        header label:hover,
+        footer a:hover {
+            text-decoration: wavy underline;
+            cursor: pointer;
+        }
+        
+        #main {
+            min-height: 77.5vh;
+            max-height: 77.5vh;
+        }
+        
+        #presents {
+            margin: 0.5rem 0;
+        }
+        
+        .collection {
+            width: 25rem;
+            max-width: 75%;
+        }
+        
+        #footer,
+        .mousedragscrollable,
+        .change #cover,
+        .change #greeting,
+        .change #server,
+        #print {
+            display: none;
+        }
+        
+        @media screen and (max-width: 1250px) {
+            #images {
+                top: 45%;
+            }
+            #images .list_item {
+                margin: 1.75vh 0;
+            }
+            #main {
+                min-height: 77.5vh;
+                max-height: 77.5vh;
+            }
+        }
+        
+        @media screen and (max-width: 750px) {
+            #images {
+                top: 40%;
+            }
+            #images .list_item {
+                margin: 1.25vh 0;
+            }
+            #main {
+                min-height: 75vh;
+                max-height: 75vh;
+            }
+        }
+        
+        @media print {
+            #images {
+                top: 42.5%;
+            }
+            #images .list_item {
+                margin: 2.5vh 0;
+            }
+            #address,
+            #server {
+                display: none;
+            }
+            #footer,
+            #print {
+                display: block;
+            }
+            #greeting {
+                z-index: -1;
+            }
+            #greeting p {
+                font-size: 1rem;
+            }
+            #main {
+                min-height: 87vh;
+                max-height: 87vh;
+            }
+        }
+    </style>
+</head>
+
+<body id="open">
+
+    <header id="header">
+        <marquee id="marquee">
+            ここをクリックすると、（）が所有するもの一覧が表示されます。
+        </marquee>
+        <nav id="nav">
+            <h1>ホームページのタイトル</h1>
+            <span id="presents">
+                <img src="https://creative-community.space/org/qr.png" width="50rem">
+            </span>
+            <form>
+                <ol class="search-box">
+                    <li>絞り込み</li>
+                    <li>
+                        <input type="radio" name="org" value="bought" id="bought">
+                        <label for="bought" class="label">bought</label>
+                    </li>
+                    <li>
+                        <input type="radio" name="org" value="gift" id="gift">
+                        <label for="gift" class="label">gift</label>
+                    </li>
+                    <li>
+                        <input type="radio" name="org" value="free" id="free">
+                        <label for="free" class="label">free or found</label>
+                    </li>
+                    <li>
+                        <input type="radio" name="org" value="made" id="made">
+                        <label for="made" class="label">made</label>
+                    </li>
+                    <li>
+                        <input type="radio" name="org" value="collaborations" id="collaborations">
+                        <label for="collaborations" class="label">collaborations</label>
+                    </li>
+                    <li>
+                        <input type="radio" name="org" value="other" id="other">
+                        <label for="other" class="label">other</label>
+                    </li>
+                    <li>
+                        <input type="radio" name="org" value="sale" id="sale">
+                        <label for="sale" class="label">$$$ FOR SALE $$$</label>
+                    </li>
+                    <li class="reset">
+                        <input type="reset" name="reset" value="View All" class="reset-button label" onclick="greeting()">
+                    </li>
+                </ol>
+            </form>
+        </nav>
+    </header>
+
+    <main id="main">
+        <div id="cover">
+            <ol id="images" class="org">
+                <?php if (!empty($rows)): ?>
+                <?php foreach ($rows as $row): ?>
+                <li class="list_item list_toggle <?=h($row[1])?>" data-org="<?=h($row[0])?>">
+                    <img src="<?=h($row[2])?>">
+                </li>
+                <?php endforeach; ?>
+                <?php else: ?>
+                <li class="list_item list_toggle min" data-org="test">
+                    <img src="/logo.png">
+                </li>
+                <?php endif; ?>
+            </ol>
+        </div>
+        <div id="server">
+            <p>
+                <?php
+                    echo $_SERVER['SERVER_PROTOCOL'];
+                ?>
+                <?php
+                    echo $_SERVER['HTTPS'];
+                ?>
+                <?php
+                    echo $_SERVER['SERVER_ADDR'];
+                ?>
+                <br/>
+                <?php
+                    echo $_SERVER['SERVER_NAME'];
+                    echo $_SERVER['REQUEST_URI'];
+                ?>
+            </p>
+        </div>
+        <ul class="mousedragscrollable">
+            <li id="about" class="collection"></li>
+            <li id="test" class="collection"></li>
+            <li id="img" class="collection"></li>
+        </ul>
+    </main>
+
+    <footer id="footer">
+        <address id="print">
+            <span class="cc_style">
+                <?php
+                echo $_SERVER['HTTP_HOST'];
+                echo $_SERVER['REQUEST_URI'];
+                ?>
+            </span>
+        </address>
+    </footer>
+
+    <script type="text/javascript ">
+        let marquee = document.querySelector('#marquee');
+        let box = document.querySelector('#open');
+
+        let btnToggleclass = function(el) {
+            el.classList.toggle('change');
+        }
+
+        marquee.addEventListener('click', function() {
+            btnToggleclass(box);
+        }, false);
+
+        $('a[href^="# "]').click(function() {
+            var href = $(this).attr("href ");
+            var target = $(href == "#" || href == " " ? 'html' : href);
+            return false;
+        });
+
+        $(function() {
+            $("#about").load("about.php");
+            $("#test").load("test/index.php");
+            $("#img").load("img.php");
+        })
+    </script>
 </body>
+
 </html>
